c5cde3ce4e27eb7439721b10d131b2f03726d979
kevin harold <harold.dev1993@gmail.com>
2023-01-06 14:10:07 -0500

    {

--- 01-dom/demoClase/index.js
+++ 01-dom/demoClase/index.js
@@ -1,1 +1,27 @@
-document.getElementById('parrafo1')
+const agregar = document.querySelector('#agregar')
+
+// function clickHandler(event){
+//     console.log(event.target)
+// }
+function cambiarColor(event) {
+    const elemento = event.target
+    
+    if(elemento.className === 'recuadro') {
+        elemento.className = 'recuadroRojo'
+    } else {
+        elemento.className = 'recuadro'
+    }
+}
+
+function clickHandler(){
+    const contenedor = document.querySelector('#contenedor')
+    
+    const newDiv = document.createElement('div')
+    newDiv.innerText = 'Kevin'
+    newDiv.className = 'recuadro'
+    newDiv.addEventListener('click', cambiarColor)
+
+
+    contenedor.appendChild(newDiv)
+}
+agregar.addEventListener('click', clickHandler)
